fbf5a49621e00f953d36f22b135a559719d32e52
Vishal Tiwari <112810406+VKT2003@users.noreply.github.com>
2024-06-08 12:35:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cors(
 ));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://vishaltiwariup2019:<password>@cluster0.8yuode6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+mongoose.connect('mongodb+srv://vishaltiwariup2019:vishal@9293@cluster0.8yuode6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
     console.log('Connected to MongoDB');
 }
 ).catch((err) => {
