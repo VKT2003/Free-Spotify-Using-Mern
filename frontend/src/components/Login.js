90c6584433b1aae586c5f92c332aa96d99fd9f65
Vishal Tiwari <112810406+VKT2003@users.noreply.github.com>
2024-06-26 20:22:48 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,90 +1,63 @@
-import React from 'react'
-import './FormStyles.css';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GoogleLogin } from '@react-oauth/google';
-import {jwtDecode} from 'jwt-decode';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { FaSpotify } from 'react-icons/fa';
+import { jwtDecode } from 'jwt-decode';
+import './App.css';
 
-const Login = () => {
+const Navbar = () => {
+  const [userData, setUserData] = useState('');
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
-
-  const handlelogin = async (e) => {
-    e.preventDefault();
-    const newUser = { email, password };
-    try {
-      const response = await axios.post('http://localhost:4000/login', newUser);
-      if (response.data.token) {
-        localStorage.setItem('token', response.data.token);
-        toast.success('Login successful');
-        setTimeout(() => {
-          navigate('/home');
-        }, 2000);
-      } else {
-        toast.error(response.data.message);
-        navigate('/login');
-      }
-    } catch (error) {
-      toast.error('Login failed');
-      console.error('Login error:', error);
+  // Fetch user data from local storage
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      // Decode token
+      const data = jwtDecode(token);
+      setUserData(data.user.email);
+    } else {
+      console.log('User not logged in');
     }
-  }
+  }, []);
 
-  const googlLogin = async (credentialResponse) => {
-    try {
-      const response = jwtDecode(credentialResponse.credential);
-      const newUser = { email: response.email, name: response.name, password: response.sub };
-      const loginResponse = await axios.post('http://localhost:4000/api/google', newUser);
-      if (loginResponse.data.token) {
-        localStorage.setItem('token', loginResponse.data.token);
-        toast.success('Login successful');
-        setTimeout(() => {
-          navigate('/home');
-        }, 2000);
-      } else {
-        toast.error(loginResponse.data.message);
-        navigate('/login');
-      }
-    } catch (error) {
-      toast.error('Login failed');
-      console.error('Login error:', error);
-      navigate('/login');
-    }
+  // Toggle menu
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
-    <div className="form-container">
-      <h2>Login</h2>
-      <form onSubmit={handlelogin}>
-        <div className="form-group">
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+    <nav className="navbar">
+      <FaSpotify size={45} color="white" />
+      <div className='split'>
+      <ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
+        <li><Link to="home">Home</Link></li>
+        <li><Link to="about">About</Link></li>
+        <li><Link to="joke">Joke</Link></li>
+      </ul>
+      {userData ? (
+        <div className="userInfo">
+          <div className="profile">{userData.charAt(0)}</div>
+          <button className="logoutbutton" onClick={() => {
+            localStorage.removeItem('token');
+            window.location.reload();
+          }}>Logout</button>
         </div>
-        <div className="form-group">
-          <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        </div>
-        <button type="submit" className="btn">Login</button>
-        Don't have an account? <Link className='linkfor' to='/register'>Sign Up</Link>
-        <GoogleOAuthProvider clientId="471238106398-q9bm3uh902sm01l62oo7kbjj2359a3al.apps.googleusercontent.com">
-          <GoogleLogin
-            onSuccess={googlLogin}
-            onError={() => {
-              toast.error('Login Failed');
-            }}
-          />
-        </GoogleOAuthProvider>
-      </form>
-      <ToastContainer />
-    </div>
-  )
-}
+      ) : (
+       <Link style={{textDecoration:'none',color:'white',fontSize:'20px',marginRight:'10px',marginTop:'4px',marginLeft:'10px'
+       }} to="login">Sign In</Link>
+      )}
+      </div>
 
-export default Login;
+      <div
+        className={`hamburgerMenu ${menuOpen ? 'active' : ''}`}
+        onClick={toggleMenu}
+      >
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
