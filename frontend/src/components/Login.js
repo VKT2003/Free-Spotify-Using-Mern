d96cd8513df6577b9f2681bb6022283bc100d931
Vishal Tiwari <112810406+VKT2003@users.noreply.github.com>
2024-06-26 20:22:26 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from 'react'
 import './FormStyles.css';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import {jwtDecode} from 'jwt-decode';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
@@ -16,49 +20,47 @@ const Login = () => {
     e.preventDefault();
     const newUser = { email, password };
     try {
-      const response = await axios.post('https://free-spotify-using-mern.vercel.app/login', newUser);
-      // Handle JWT token
+      const response = await axios.post('http://localhost:4000/login', newUser);
       if (response.data.token) {
-        console.log(response.data.token);
         localStorage.setItem('token', response.data.token);
-        toast.success('Login successful', {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-        navigate('/home');
+        toast.success('Login successful');
+        setTimeout(() => {
+          navigate('/home');
+        }, 2000);
       } else {
-        toast.error(response.data.message, {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
+        toast.error(response.data.message);
         navigate('/login');
       }
     } catch (error) {
-      toast.error('An error occurred. Please try again later.', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
+      toast.error('Login failed');
+      console.error('Login error:', error);
     }
   }
 
+  const googlLogin = async (credentialResponse) => {
+    try {
+      const response = jwtDecode(credentialResponse.credential);
+      const newUser = { email: response.email, name: response.name, password: response.sub };
+      const loginResponse = await axios.post('http://localhost:4000/api/google', newUser);
+      if (loginResponse.data.token) {
+        localStorage.setItem('token', loginResponse.data.token);
+        toast.success('Login successful');
+        setTimeout(() => {
+          navigate('/home');
+        }, 2000);
+      } else {
+        toast.error(loginResponse.data.message);
+        navigate('/login');
+      }
+    } catch (error) {
+      toast.error('Login failed');
+      console.error('Login error:', error);
+      navigate('/login');
+    }
+  };
+
   return (
     <div className="form-container">
-      <ToastContainer />
       <h2>Login</h2>
       <form onSubmit={handlelogin}>
         <div className="form-group">
@@ -71,10 +73,18 @@ const Login = () => {
         </div>
         <button type="submit" className="btn">Login</button>
         Don't have an account? <Link className='linkfor' to='/register'>Sign Up</Link>
+        <GoogleOAuthProvider clientId="471238106398-q9bm3uh902sm01l62oo7kbjj2359a3al.apps.googleusercontent.com">
+          <GoogleLogin
+            onSuccess={googlLogin}
+            onError={() => {
+              toast.error('Login Failed');
+            }}
+          />
+        </GoogleOAuthProvider>
       </form>
+      <ToastContainer />
     </div>
   )
 }
 
 export default Login;
-    
